6e01a75d59a37a1277c64fe96601b1937ee876fe
J2b2590 <31115299+J2b2590@users.noreply.github.com>
2019-07-12 13:29:35 -0500

    second

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+console.log("Connected")
+
+
+// $('input[type="checkbox"]').on('change', function(){
+//   $('input[type="checkbox"]').not(this).prop('checked', false);
+//   console.log(this)
+// });
